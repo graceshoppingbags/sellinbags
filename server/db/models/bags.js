7de10f22f47f371c2afce0767edc20b18ea39f37
Brendan Fallon <brendanc.fallon@gmail.com>
2019-04-16 11:35:33 -0500

    added bags model

---
+++ server/db/models/bags.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Bags = db.define('bags', {
+    style: {
+        type: Sequelize.STRING, 
+        allowNull: false
+    },
+    stripeOneColor: {
+        type: Sequelize.STRING,
+    },
+    stripeTwoColor: {
+        type: Sequelize.STRING
+    },
+    stripeThreeColor: {
+        type: Sequelize.STRING
+    },
+    material: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+})
+
