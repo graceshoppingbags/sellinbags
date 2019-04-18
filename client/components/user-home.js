ada4ccfcd2b9606d6fac5465bbc2a9645ccc9981
Brendan Fallon <brendanc.fallon@gmail.com>
2019-04-18 10:41:47 -0500

    rendering previous user orders on user home

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Orders from './orders'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  console.log(props)
-  const {email} = props
+  const {email, orders} = props
 
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <ul>Here are your previous orders:
+        <Orders  orders={orders} ></Orders>
+      </ul>
     </div>
   )
 }
